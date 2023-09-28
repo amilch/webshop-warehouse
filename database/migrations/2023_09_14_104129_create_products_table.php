caa17e7094bddfa7f6d75e9dba28ad65db2cd23b
Your Name <user@laptop.fritz.box>
2023-09-28 04:07:11 +0200

    add unique to sku column in table

--- database/migrations/2023_09_14_104129_create_products_table.php
+++ database/migrations/2023_09_14_104129_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('sku');
+            $table->string('sku')->unique();
             $table->integer('quantity');
             $table->integer('reserved');
             $table->timestamps();
